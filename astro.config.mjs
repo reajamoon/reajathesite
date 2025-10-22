80358ef94745bd402366f32e7ac578ca4f391161
reaja <reajamoon@pm.me>
2025-10-22 13:44:01 -0700

    bg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
+
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+      service: passthroughImageService()
+  },
+
+  integrations: [icon()]
+});
---
+++ src/pages/cool.md
@@ -0,0 +1,6 @@
+---
+title: cool page
+---
+
+# Neato Markdown
+Wheeee
